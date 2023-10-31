540122ea09f12efff3ebd9b4f38624f074a1a916
Perry Randall <perryjrandall@gmail.com>
2023-10-31 16:01:05 -0700

    [forge] Unstrip only the forge binary
    This was done because it was simple, however stripping forge binary ruins debuggability (not that it was easy to debug for starters)
    
    So explicitly do not strip forge for now
    
    Test Plan: check the forge binary in the docker image is not stripped, but that all other clis remain stripped

--- docker/builder/build-tools.sh
+++ docker/builder/build-tools.sh
@@ -4,17 +4,30 @@
 set -e
 
 PROFILE=cli
+# TODO use an unstripped cli profile, we dont need to use release image for
+# forge cli, that might save build time / space without losing backtrace
+FORGE_PROFILE=release
 
 echo "Building tools and services docker images"
 echo "PROFILE: $PROFILE"
+echo "FORGE_PROFILE: $PROFILE"
 echo "CARGO_TARGET_DIR: $CARGO_TARGET_DIR"
+
+cargo build --locked --profile=$FORGE_PROFILE \
+    -p aptos-forge-cli \
+    "$@" &
+
+pid=$!
+
+trap "echo 'ensuring cargo is stopped' && ps $pid && kill $pid" EXIT
+
+echo "Forge running as $pid"
 
 # Build all the rust binaries
 cargo build --locked --profile=$PROFILE \
     -p aptos \
     -p aptos-backup-cli \
     -p aptos-faucet-service \
-    -p aptos-forge-cli \
     -p aptos-fn-check-client \
     -p aptos-node-checker \
     -p aptos-openapi-spec-generator \
@@ -38,7 +51,6 @@ BINS=(
     aptos-telemetry-service
     aptos-fn-check-client
     aptos-debugger
-    forge
     aptos-transaction-emitter
     aptos-indexer-grpc-cache-worker
     aptos-indexer-grpc-file-store
@@ -53,6 +65,12 @@ mkdir dist
 for BIN in "${BINS[@]}"; do
     cp $CARGO_TARGET_DIR/$PROFILE/$BIN dist/$BIN
 done
+
+echo "Waiting for forge build to finish"
+wait $pid
+
+# Explicitly copy forge because it uses a different profile
+cp $CARGO_TARGET_DIR/$FORGE_PROFILE/forge dist/forge
 
 # Build the Aptos Move framework and place it in dist. It can be found afterwards in the current directory.
 echo "Building the Aptos Move framework..."
